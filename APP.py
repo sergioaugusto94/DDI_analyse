e681819a12c590319da53d55b8b56a1adc47ec23
Sérgio Costa <82229175+sergioaugusto94@users.noreply.github.com>
2022-01-04 09:46:33 -0300

    Update APP.py
    Adding data viewing and button to plot the data

--- APP.py
+++ APP.py
@@ -9,3 +9,13 @@ st.sidebar.markdown('To use the app, click on button below to ' +
                     'upload your DDI database.')
 
 file = st.sidebar.file_uploader("# Upload the data", type=['xlsx'])
+
+
+if file is not None:
+    data = pd.read_csv(file)
+    st.dataframe(data)
+    st.markdown('This is the loaded data')
+    run_button = st.button('Plot')
+    run_state = button_states()
+
+    
