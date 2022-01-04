56c4a10a47c5a96a04b264545b08df6a0eea01fc
Sérgio Costa <82229175+sergioaugusto94@users.noreply.github.com>
2022-01-04 11:44:43 -0300

    Update APP.py

--- APP.py
+++ APP.py
@@ -10,27 +10,6 @@ st.sidebar.markdown('To use the app, click on button below to ' +
 
 file = st.sidebar.file_uploader("# Upload the data", type=['xlsx'])
 
-#--------Variables-Units-Precision-------
-units = data.iloc[0,:]
-precision = data.iloc[1,:]
-variables = data.columns
-#---------------------------
-
-#--------Removing Empty Rows and Defining Conditions that were ran-------
-data.dropna(inplace = True, subset = [data.columns[1]])
-data['Condition'] = data[['CL_dSpeed', 'CL_BMEP SI', 'CL_Throttle', 
-                          'n VVT_ICL1_DIAL_CL_VAL', 
-                          'n DL_SPK_ADV', 
-                          'n VVL_STATE_ACT']].astype(str).agg('_'.join, axis=1)
-#---------------------------
-
-#-----Defining the date that the test were ran--------------
-data['Date'] = (data['TimeStamp'].str.slice(start=20) + 
-                data['TimeStamp'].str.slice(start=3, stop=10) + 
-                data['TimeStamp'].str.slice(start=10, stop=19))
-
-data['Date'] = pd.to_datetime(data['Date'], format='%Y %b %d %H:%M:%S')
-#---------------------------
 
 #-----Display the dataset-------
 @st.cache(allow_output_mutation=True)
@@ -38,7 +17,32 @@ def button_states():
     return {"pressed": False}
 
 if file is not None:
+    #--------Loading dataset-------
     data = pd.read_excel(file, sheet_name='Channel Info')
+    #---------------
+
+    #--------Variables-Units-Precision-------
+    units = data.iloc[0,:]
+    precision = data.iloc[1,:]
+    variables = data.columns
+    #---------------------------
+
+    #--------Removing Empty Rows and Defining Conditions that were ran-------
+    data.dropna(inplace = True, subset = [data.columns[1]])
+    data['Condition'] = data[['CL_dSpeed', 'CL_BMEP SI', 'CL_Throttle', 
+                              'n VVT_ICL1_DIAL_CL_VAL', 
+                              'n DL_SPK_ADV', 
+                              'n VVL_STATE_ACT']].astype(str).agg('_'.join, axis=1)
+    #---------------------------
+
+    #-----Defining the date that the test were ran--------------
+    data['Date'] = (data['TimeStamp'].str.slice(start=20) + 
+                    data['TimeStamp'].str.slice(start=3, stop=10) + 
+                    data['TimeStamp'].str.slice(start=10, stop=19))
+
+    data['Date'] = pd.to_datetime(data['Date'], format='%Y %b %d %H:%M:%S')
+    #---------------------------
+    
     st.dataframe(data)
     st.markdown('This is the loaded data')
     run_button = st.button('Plot')
