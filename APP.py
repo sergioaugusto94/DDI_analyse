c6bb4296bee15267b4829ac8763e1c58d98f7d55
Sérgio Costa <82229175+sergioaugusto94@users.noreply.github.com>
2022-01-07 08:53:15 -0300

    Update APP.py
    Adding a session_state to increase app performance

--- APP.py
+++ APP.py
@@ -46,21 +46,24 @@ st.sidebar.markdown('To use the app, click on button below to ' +
 
 file = st.sidebar.file_uploader("# Upload the data", type=['xlsx'])
 
+if 'run_num' not in st.session_state:
+	st.session_state.run_num = 0
 
 if file is not None:
-	data = data_processing(file)
-	
+	if run_num == 0:
+		data = data_processing(file)
+
+		#-----Creating different dataframes for each engine operation-------
+		points = data['Condition'].unique()
+
+		for x in range(1, 1 + len(data.groupby('Condition').count().iloc[:, 1])):
+			globals()['df%s' % x] = data.where(data['Condition'] == 
+							points[x-1]).dropna(subset=['File Name'])
+		
 	st.markdown('This is the loaded data')
 	st.dataframe(data)
-
-	#-----Creating different dataframes for each engine operation-------
-	points = data['Condition'].unique()
-
-	for x in range(1, 1 + len(data.groupby('Condition').count().iloc[:, 1])):
-		globals()['df%s' % x] = data.where(data['Condition'] == 
-					   	points[x-1]).dropna(subset=['File Name'])
 		
-				#-----Drop down list for each variable-------
+	#-----Drop down list for each variable-------
 	form1 = st.form(key='Options')
 	
 	option = form1.selectbox(
@@ -86,6 +89,7 @@ if file is not None:
 
 	#if plot_state['pressed']:
 	if plot_button:
+		st.session_state.run_num = 1
 		fig = pf.plot(df_plot, var)
 		st.plotly_chart(fig)
 
