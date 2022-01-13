bf67df7963a12e8859bc3f35915df6b957718d41
Sérgio Costa <82229175+sergioaugusto94@users.noreply.github.com>
2022-01-13 11:53:05 -0300

    Update APP.py

--- APP.py
+++ APP.py
@@ -58,9 +58,9 @@ if st.session_state.run_num == 0:
 	st.session_state.file_save = st.sidebar.file_uploader("# Upload the data", type=['xlsx'])
 
 
+n_data = st.number_input('Points to Analyse', min_value=0, value=int(500))
 
 if st.session_state.file_save is not None:
-	n_data = st.number_input('Points to Analyse', min_value=0, value=int(500))
 	if st.session_state.run_num == 0:
 		data = data_processing(st.session_state.file_save, n_data)
 		st.session_state.data_save = data
