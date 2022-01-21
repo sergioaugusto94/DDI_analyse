a51abf12de920a563eef800f9cb202602165864d
Sérgio Costa <82229175+sergioaugusto94@users.noreply.github.com>
2022-01-21 11:07:15 -0300

    Update APP.py

--- APP.py
+++ APP.py
@@ -97,6 +97,10 @@ if st.session_state.file_save is not None:
 	df_plot = st.session_state.data_save.where(st.session_state.data_save['Condition'] == 
 					   	option2).dropna(subset=['File Name'])
 	
+	df_plot2 = df_plot.drop(df_plot[df_plot.columns].std()[df_plot[df_plot.columns].std()<0.001].index, axis=1)
+
+	var_plot = form1.multiselect(
+	'Choose the variable to be ploted', df_plot2.columns.values.to_list())
 	
 	plot_button = form1.form_submit_button('Plot')
 	plot_state = button_states()
