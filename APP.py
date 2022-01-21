7f28dd30ad34c930c172aee8e2999f51e0c22c6e
Sérgio Costa <82229175+sergioaugusto94@users.noreply.github.com>
2022-01-21 09:25:27 -0300

    Removing Columns with low std

--- APP.py
+++ APP.py
@@ -10,6 +10,7 @@ import plot_function as pf
 def data_processing(file):
 	#--------Loading dataset-------
 	data = pd.read_excel(file, sheet_name='Channel Info')
+	data.drop(data[data.columns].std()[data[data.columns].std()<0.001].index, axis=1)
 
 	#--------Variables-Units-Precision-------
 	units = data.iloc[0,:]
